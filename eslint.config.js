121ede9d87ba5c2fb8cfc47cc0b853a02a7a1be7
Yu Zheng <yu.zheng@getcheddar.com.au>
2024-03-22 07:51:16 +1100

    Initial commit
    Generated by create-expo-app 2.1.1.

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = [
+  'universe/native',
+  // Any other config imports go at the top
+  eslintPluginPrettierRecommended,
+];
