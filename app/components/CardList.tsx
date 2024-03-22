3b4a7c7fe4cae27678870281a7f21bbdf9326d44
Yu Zheng <yu.zheng@getcheddar.com.au>
2024-03-22 22:02:45 +1100

    fix index calculator

--- app/components/CardList.tsx
+++ app/components/CardList.tsx
@@ -1,8 +1,9 @@
 import { ScrollView } from "react-native-gesture-handler"
 import { Card, cardMargin, cardWidth } from "./Card"
-import { NativeScrollEvent, NativeSyntheticEvent } from "react-native"
+import { NativeScrollEvent, NativeSyntheticEvent, StyleSheet, View } from "react-native"
 import { useMemo, useRef } from "react"
 import * as Haptics from 'expo-haptics';
+import { SliderIndicator } from "./SliderIndicator";
 
 const maxIndex = 3;
 
@@ -12,14 +13,12 @@ const getIndex = (position: number): number => {
     return maxIndex;
   }
   const baseIndex = Math.floor((position - 16) / (cardWidth + cardMargin));
-  const offsetX = position - baseIndex * (cardWidth + cardMargin);
+  const offsetX = position - baseIndex * (cardWidth + cardMargin) - ((baseIndex - 1 < 0) ? 0 : (baseIndex - 1) * cardMargin);
   if (offsetX > cardWidth / 2) {
     return baseIndex + 1;
   }
   return baseIndex
 }
-
-
 
 export const CardList = () => {
   const scrollRef = useRef<ScrollView>(null);
@@ -37,16 +36,19 @@ export const CardList = () => {
   //   });
   // }
 
+  const onMomentumScrollBegin = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const index = Math.round(getIndex(e.nativeEvent.contentOffset.x));
+    console.log('onMomentumScrollBegin: index: ', index);
+  }
+
   const onMomentumScrollEnd = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
-    const index = Math.round(e.nativeEvent.contentOffset.x / cardWidth);
-    console.log('index: ', index);
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
   }
 
   const snapToOffsets = useMemo(() => Array.from(Array(maxIndex), (_, i) => cardWidth * i + ((i - 1 < 0) ? 0 : cardMargin * (i-1))), []);
 
-  console.log('snapToOffsets: ', snapToOffsets);
   return (
+    <View>
     <ScrollView
       ref={scrollRef}
       snapToInterval={334}
@@ -54,11 +56,13 @@ export const CardList = () => {
       decelerationRate={'fast'} 
       horizontal showsHorizontalScrollIndicator={false} 
       // onScrollEndDrag={onScrollEndDrag} 
+      onMomentumScrollBegin={onMomentumScrollBegin}
       onMomentumScrollEnd={onMomentumScrollEnd}>
       <Card index={0}></Card>
       <Card index={1}></Card>
       <Card index={2}></Card>
       <Card index={3} last></Card>
     </ScrollView>
+    </View>
   )
 }
